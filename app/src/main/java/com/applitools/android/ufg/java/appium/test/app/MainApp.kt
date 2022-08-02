e8c1cb3918abf6b7f08fe576d5e96894b25d8429
alexander-kachechka <alexander.kachechka@applitools.com>
2022-08-02 10:27:09 +0300

    Updated NMG library to the latest version

--- app/src/main/java/com/applitools/android/ufg/java/appium/test/app/MainApp.kt
+++ app/src/main/java/com/applitools/android/ufg/java/appium/test/app/MainApp.kt
@@ -1,14 +1,12 @@
 package com.applitools.android.ufg.java.appium.test.app
 
 import android.app.Application
-import com.applitools.eyes.Logger
 import com.applitools.vhs.EyesUfg
 
 class MainApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val logger = Logger()
-        EyesUfg.setup(this, logger, "API_KEY_HERE", EyesUfg.Mode.PULL_VHS_MODE);
+        EyesUfg.setup(this, EyesUfg.Mode.APPIUM)
     }
 }
